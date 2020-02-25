df65db2284a33b6bdc3e6c66622a1be47c93d898
hausmanne <54555072+hausmanne@users.noreply.github.com>
2020-02-25 15:37:32 +0100

    Application finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :todo_lists do
+    resources :todo_items do
+      member do
+        patch :complete
+      end
+    end
+  end
+  root "todo_lists#index"
 end
